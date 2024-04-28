d105140065b7c0d84ce244ffd7eeb79ce4e0326b
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-04-28 20:09:31 +0300

    move api/core/Database.php to db/database.php

---
+++ db/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DB;
+
+// Database Singleton Class
+class Database
+{
+  private $dbh = null;
+  private static $instance = null;
+
+  private function __construct()
+  {
+    $config = require('config/database.php');
+
+    $dsn = "mysql:host={$config['DB_HOST']};port={$config['DB_PORT']};dbname={$config['DB_NAME']};charset={$config['DB_CHARSET']}";
+
+    $this->dbh = new \PDO($dsn, $config['DB_USERNAME'], $config['DB_PASSWORT'], $config['DB_OPTIONS']);
+  }
+  private static function GetConnection()
+  {
+    if (self::$instance == null)
+    {
+      self::$instance = new Database();
+    }
+    return self::$instance->dbh;
+  }
+
+  public static function Query($query)
+  {
+    $stmt = self::GetConnection()->prepare($query);
+    $stmt->execute();
+
+    return $stmt;
+  }
+}
