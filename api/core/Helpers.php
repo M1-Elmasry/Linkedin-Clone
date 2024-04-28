ea6e8bbca4eb01835343fb3d2811d972a58573e9
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-04-28 20:42:25 +0300

    replace helpers.php with utils class and make custom uuid function

--- api/core/Helpers.php
+++ api/core/Helpers.php
@@ -1,12 +1,1 @@
-<?php
 
-function base_path($path)
-{
-  return $_SERVER['DOCUMENT_ROOT'] . "/" .$path;
-}
-
-function abort($code = 404)
-{
-  http_response_code($code);
-  echo "Status Code: {$code}";
-}
