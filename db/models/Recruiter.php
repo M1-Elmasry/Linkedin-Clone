d2f6fa8d387a1baaa95d998d9b396373277bc318
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-05-02 04:23:26 +0300

    edit constrcutor to allow making an obj without creating new record in db & implement update methods

--- db/models/Recruiter.php
+++ db/models/Recruiter.php
@@ -8,12 +8,12 @@ require('utils/utils.php');
 use DB\Models\User as User;
 use DB\Database as Database;
 use utils\Utils as Utils;
-use UnexpectedValueException;
 
 class Recruiter extends User
 {
     private $id;
-    private $ImagePath;
+    private $createdAt;
+    private $updatedAt;
 
     public function __construct(
         string $imagePath,
@@ -25,12 +25,14 @@ class Recruiter extends User
         string $industry,
         string $title,
         string $currentCompany,
-        string $about
+        string $about,
+        bool $is_new = true, // this for encapsulate obj without create new record in db
+        string $id = null,  // dependency for $is_new
+        string $createdAt = null, // dependency for $is_new
+        string $updatedAt = null, // dependency for $is_new
     ) {
-        $this->id = Utils::generateUUID();
-        $this->setImagePath($imagePath);
-
         parent::__construct(
+            $imagePath,
             $Fname,
             $Lname,
             $email,
@@ -42,26 +44,50 @@ class Recruiter extends User
             $about
         );
 
-        $this->createdAt = date("Y-m-d H:i:s");
-        $this->updatedAt = $this->createdAt;
+        if ($is_new) {
+            $this->id = Utils::generateUUID();
+            $this->createdAt = date("Y-m-d H:i:s");
+            $this->updatedAt = $this->createdAt;
 
-        Database::Query("
-        INSERT INTO
-        recruiters(id, image, first_name, last_name, email, phone, password,
-              industry, current_company, title, about, is_recruiter, created_at, updated_at)
-        VALUES('$this->id', '$this->ImagePath', '$this->Fname', '$this->Lname', '$this->email', '$this->pswd',
-               '$this->phone', '$this->industry', '$this->currentCompany', '$this->title', '$this->about', TRUE,
-               '$this->createdAt', '$this->updatedAt');");
-
+            Database::Query("
+              INSERT INTO
+              recruiters(id, image, first_name, last_name, email, phone, password,
+                    industry, current_company, title, about, is_recruiter, created_at, updated_at)
+              VALUES('$this->id', '$this->ImagePath', '$this->Fname', '$this->Lname', '$this->email', '$this->pswd',
+                     '$this->phone', '$this->industry', '$this->currentCompany', '$this->title', '$this->about', TRUE,
+                     '$this->createdAt', '$this->updatedAt');
+            ");
+        } else {
+            $this->id = $id;
+            $this->createdAt = $createdAt;
+            $this->updatedAt = $updatedAt;
+        }
     }
 
-    public function setImagePath($path): void
+    public static function getRecruiterById(string $RecruiterId): Recruiter | null
     {
-        if (!empty($path) && strlen($path) <= 256) {
-            $this->ImagePath = $path;
-        } else {
-            throw new UnexpectedValueException('$newPath cannot be empty and more than 256 char');
+        $record = Database::Query("SELECT * FROM recruiters WHERE id = '$RecruiterId' ")->fetch();
+
+        if (empty($record)) {
+            return null;
         }
+
+        return new Recruiter(
+            $record['image'],
+            $record['first_name'],
+            $record['last_name'],
+            $record['email'],
+            $record['phone'],
+            $record['password'],
+            $record['industry'],
+            $record['title'],
+            $record['current_company'],
+            $record['about'],
+            false,
+            $record["id"],
+            $record["created_at"],
+            $record["updated_at"]
+        );
     }
 
     public function getAllData(): array
@@ -69,7 +95,62 @@ class Recruiter extends User
         return [
           "id" => $this->id,
           "imagePath" => $this->ImagePath,
-          ...$this->getData()
+          ...$this->getData(),
+          "createdAt" => $this->createdAt,
+          "updatedAt" => $this->updatedAt
         ];
+    }
+
+    public function updateImagePath(string $newImagePath)
+    {
+        $this->updatedAt = $this->upImgPath($newImagePath, "recruiters", $this->id);
+        $this->ImagePath = $newImagePath;
+    }
+
+    public function updatePassword(string $newPassword)
+    {
+        $this->updatedAt = $this->upPswd($newPassword, "recruiters", $this->id);
+        $this->pswd = $newPassword;
+    }
+
+    public function updateData(
+        string $Fname = null,
+        string $Lname = null,
+        string $email = null,
+        string $phone = null,
+        string $industry = null,
+        string $title = null,
+        string $currentCompany = null,
+        string $about = null,
+    ) {
+        $Fname = $Fname ?? $this->Fname;
+        $Lname = $Lname ?? $this->Lname;
+        $email = $email ?? $this->email;
+        $phone = $phone ?? $this->phone;
+        $industry = $industry ?? $this->industry;
+        $title = $title ?? $this->title;
+        $currentCompany = $currentCompany ?? $this->currentCompany;
+        $about = $about ?? $this->about;
+
+        $this->updatedAt = $this->upData(
+            $Fname,
+            $Lname,
+            $email,
+            $phone,
+            $industry,
+            $title,
+            $currentCompany,
+            $about,
+            "recruiters",
+            $this->id
+        );
+        $this->Fname = $Fname;
+        $this->Lname = $Lname;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->industry = $industry;
+        $this->title = $title;
+        $this->currentCompany = $currentCompany;
+        $this->about = $about;
     }
 }
