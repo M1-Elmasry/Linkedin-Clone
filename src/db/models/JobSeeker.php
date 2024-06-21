d03e363da4a72b6d6f7534ff48d2a0ff808c5e91
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-06-21 22:44:34 +0300

    add address fields to User model

--- src/db/models/JobSeeker.php
+++ src/db/models/JobSeeker.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DB\Models;
 
 use \DB\Models\User;
@@ -6,70 +7,73 @@ use \DB\Database;
 
 class JobSeeker extends User
 {
-    public function __construct(
-        string $Fname,
-        string $Lname,
-        string $email,
-        string $pswd,
-        bool $is_new = false, // this for encapsulate obj without create new record in db
-        string $imagePath = '-',
-        string $phone = '-',
-        string $industry = '-',
-        string $title = '-',
-        string $currentCompany = '-',
-        string $about = '-',
-        string $id = null,  // dependency for $is_new
-        string $createdAt = null, // dependency for $is_new
-        string $updatedAt = null, // dependency for $is_new
-    ) {
-        parent::__construct(
-            $Fname,
-            $Lname,
-            $email,
-            $pswd,
-            false,
-            $imagePath,
-            $phone,
-            $industry,
-            $title,
-            $currentCompany,
-            $about
-        );
-        
-        $this->tableName = "job_seekers";
+  public function __construct(
+    string $Fname,
+    string $Lname,
+    string $email,
+    string $address,
+    string $pswd,
+    bool $is_new = false, // this for encapsulate obj without create new record in db
+    string $imagePath = '-',
+    string $phone = '-',
+    string $industry = '-',
+    string $title = '-',
+    string $currentCompany = '-',
+    string $about = '-',
+    string $id = null,  // dependency for $is_new
+    string $createdAt = null, // dependency for $is_new
+    string $updatedAt = null, // dependency for $is_new
+  ) {
+    parent::__construct(
+      $Fname,
+      $Lname,
+      $email,
+      $address,
+      $pswd,
+      false,
+      $imagePath,
+      $phone,
+      $industry,
+      $title,
+      $currentCompany,
+      $about
+    );
 
-        if ($is_new) {
-            $this->createNewRecord();
-        } else {
-            $this->userId = $id;
-            $this->createdAt = $createdAt;
-            $this->updatedAt = $updatedAt;
-        }
+    $this->tableName = "job_seekers";
+
+    if ($is_new) {
+      $this->createNewRecord();
+    } else {
+      $this->userId = $id;
+      $this->createdAt = $createdAt;
+      $this->updatedAt = $updatedAt;
+    }
+  }
+
+  public static function getJobSeekerById(string $JobSeekerId): ?JobSeeker
+  {
+    $record = Database::Query("SELECT * FROM job_seekers WHERE id = '$JobSeekerId' ")->fetch();
+
+    if (empty($record)) {
+      return null;
     }
 
-    public static function getJobSeekerById(string $JobSeekerId): ?JobSeeker
-    {
-        $record = Database::Query("SELECT * FROM job_seekers WHERE id = '$JobSeekerId' ")->fetch();
-
-        if (empty($record)) {
-            return null;
-        }
-
-        return new JobSeeker(
-            $record['image'],
-            $record['first_name'],
-            $record['last_name'],
-            $record['email'],
-            $record['phone'],
-            $record['password'],
-            $record['industry'],
-            $record['title'],
-            $record['current_company'],
-            $record['about'],
-            false,
-            $record["id"],
-            $record["created_at"],
-            $record["updated_at"]
-        );
-    }
+    return new JobSeeker(
+      $record['image'],
+      $record['first_name'],
+      $record['last_name'],
+      $record['email'],
+      $record['address'],
+      $record['phone'],
+      $record['password'],
+      $record['industry'],
+      $record['title'],
+      $record['current_company'],
+      $record['about'],
+      false,
+      $record["id"],
+      $record["created_at"],
+      $record["updated_at"]
+    );
+  }
 }
--- src/db/models/Recruiter.php
+++ src/db/models/Recruiter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DB\Models;
 
 use \DB\Models\User;
@@ -6,70 +7,73 @@ use \DB\Database;
 
 class Recruiter extends User
 {
-    public function __construct(
-        string $Fname,
-        string $Lname,
-        string $email,
-        string $pswd,
-        bool $is_new = false, // this for encapsulate obj without create new record in db
-        string $imagePath = '-',
-        string $phone = '-',
-        string $industry = '-',
-        string $title = '-',
-        string $currentCompany = '-',
-        string $about = '-',
-        string $id = null,  // dependency for $is_new
-        string $createdAt = null, // dependency for $is_new
-        string $updatedAt = null, // dependency for $is_new
-    ) {
-        parent::__construct(
-            $Fname,
-            $Lname,
-            $email,
-            $pswd,
-            true,
-            $imagePath,
-            $phone,
-            $industry,
-            $title,
-            $currentCompany,
-            $about
-        );
+  public function __construct(
+    string $Fname,
+    string $Lname,
+    string $email,
+    string $address,
+    string $pswd,
+    bool $is_new = false, // this for encapsulate obj without create new record in db
+    string $imagePath = '-',
+    string $phone = '-',
+    string $industry = '-',
+    string $title = '-',
+    string $currentCompany = '-',
+    string $about = '-',
+    string $id = null,  // dependency for $is_new
+    string $createdAt = null, // dependency for $is_new
+    string $updatedAt = null, // dependency for $is_new
+  ) {
+    parent::__construct(
+      $Fname,
+      $Lname,
+      $email,
+      $address,
+      $pswd,
+      true,
+      $imagePath,
+      $phone,
+      $industry,
+      $title,
+      $currentCompany,
+      $about
+    );
 
-        $this->tableName = "recruiters";
+    $this->tableName = "recruiters";
 
-        if ($is_new) {
-            $this->createNewRecord();
-        } else {
-            $this->userId = $id;
-            $this->createdAt = $createdAt;
-            $this->updatedAt = $updatedAt;
-        }
+    if ($is_new) {
+      $this->createNewRecord();
+    } else {
+      $this->userId = $id;
+      $this->createdAt = $createdAt;
+      $this->updatedAt = $updatedAt;
+    }
+  }
+
+  public static function getRecruiterById(string $RecruiterId): Recruiter | null
+  {
+    $record = Database::Query("SELECT * FROM recruiters WHERE id = '$RecruiterId' LIMIT 1")->fetch();
+
+    if (empty($record)) {
+      return null;
     }
 
-    public static function getRecruiterById(string $RecruiterId): Recruiter | null
-    {
-        $record = Database::Query("SELECT * FROM recruiters WHERE id = '$RecruiterId' LIMIT 1")->fetch();
-
-        if (empty($record)) {
-            return null;
-        }
-
-        return new Recruiter(
-            $record['image'],
-            $record['first_name'],
-            $record['last_name'],
-            $record['email'],
-            $record['phone'],
-            $record['password'],
-            $record['industry'],
-            $record['title'],
-            $record['current_company'],
-            $record['about'],
-            false,
-            $record["id"],
-            $record["created_at"],
-            $record["updated_at"]
-        );
-    }
+    return new Recruiter(
+      $record['image'],
+      $record['first_name'],
+      $record['last_name'],
+      $record['email'],
+      $record['address'],
+      $record['phone'],
+      $record['password'],
+      $record['industry'],
+      $record['title'],
+      $record['current_company'],
+      $record['about'],
+      false,
+      $record["id"],
+      $record["created_at"],
+      $record["updated_at"]
+    );
+  }
 }
