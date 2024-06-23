ea6a451a708ec65ed352f0336d30e3bb506d6953
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-06-23 13:46:03 +0300

    remove un-necessary files

--- src/db/models/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('db/models/Recruiter.php');
-// require('db/models/JobSeeker.php');
-use DB\Models\Recruiter as Recruiter;
-// use DB\Models\JobSeeker as JobSeeker;
-
-$x1 = new Recruiter("public/image.jpg", "mostafa", "elmasry", "mmf@gmail.com", "asdfj*&Ynj", "+201145551878", "software development", "software Engineer", "-", "-");
